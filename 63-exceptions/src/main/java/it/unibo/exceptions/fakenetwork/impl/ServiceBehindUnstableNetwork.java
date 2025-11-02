724a87c0a7e61a127257229a8a88d59f418d8cbc
Andrea Reggiani <andrea.reggiani14@studio.unibo.it>
2025-11-02 20:33:25 +0100

    modifica a es63 gestione delle eccezioni in sendData migliorata

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -69,7 +69,7 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
              *
              * The previous exceptions must be set as the cause of the new exception
              */
-            throw new IllegalStateException(message);
+            throw new IllegalArgumentException(message);
         } 
 
     }
